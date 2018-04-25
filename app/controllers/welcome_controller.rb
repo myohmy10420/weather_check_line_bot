cddb2077bc2c9e3705c016ac4eb7dda3cabae1e2
KurtHsu <myohmy10420@gmail.com>
2018-04-25 20:59:25 +0800

    Can response

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,18 @@
 class WelcomeController < ApplicationController
 protect_from_forgery with: :null_session
   def webhook
-    # # Line Bot API 物件初始化
-    # client = Line::Bot::Client.new { |config|
-    #   config.channel_secret = ENV['line_channel_secret']
-    #   config.channel_token = ENV['line_channel_token']
-    # }
+    # Line Bot API 物件初始化
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['line_channel_secret']
+      config.channel_token = ENV['line_channel_token']
+    }
 
+    response_message = {
+      type: "text",
+      text: params["events"].first["message"]["text"]
+    }
+
+    client.reply_message(reply_token, response_message)
     head :ok
   end
 end
